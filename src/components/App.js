3de0414e5360c0aca7641a92b2a08e0e5f9e098c
BenSwartz <odanata410@gmail.com>
2022-08-01 14:26:24 -0500

    working on login route

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
-import { Home } from "./index";
+import { Home, Login, Register, Routines, Activities, MyActivities } from "./index";
 
 function App() {
     return (
         <div>
             <main>
                 <nav>
-                <Link className="tab" to="/Home">Home</Link>
-                <Link className="tab" to="/login">Login/Logout</Link>
-                <Link className="tab" to="/register">Register</Link>
-                <Link className="tab" to="/routines">Routines</Link>
-                <Link className="tab" to="/activities">Activities</Link>
-                <Link className="tab" to="/activities/me">My Activities</Link>
+                    <Link className="tab" to="/home">Home</Link>
+                    <Link className="tab" to="/login">Login/Logout</Link>
+                    <Link className="tab" to="/register">Register</Link>
+                    <Link className="tab" to="/routines">Routines</Link>
+                    <Link className="tab" to="/activities">Activities</Link>
+                    <Link className="tab" to="/activities/me">My Activities</Link>
                 </nav>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/routines" element={<Routines />} />
+                    <Route path="/activities" element={<Activities />} />
+                    <Route path="/activities/me" element={<MyActivities />} />
                 </Routes>
             </main>
         </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,46 @@
-function Login (){}
+import React from 'react'
+import { useState } from "react"
+import { userLogin } from '../api';
 
- export default Login
+function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [Error, setError] = useState('')
+
+    const SubmitHandler = async (e) => {
+        try {
+            e.preventDefault()
+            const result = await userLogin(username, password)
+            console.log(result)
+        } catch (error) {
+            console.error(error)
+            setError('Username and Password Do Not Match')
+        }
+    };
+
+    return (
+        <div>
+            <form className="login" onSubmit={SubmitHandler}>
+                <h2>Login</h2>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }} />
+                <input
+                    type="text"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }} />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
 
